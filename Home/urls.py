f10d3129e3dbea19abc7ac579fd58ad54adecd9e
tanvik22 <tanvikhasgiwal208@gmail.com>
2021-09-25 17:32:43 +0530

    Add Login and Sign Up

--- Home/urls.py
+++ Home/urls.py
@@ -4,5 +4,8 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login.html', views.login1,name = 'login1'),
     path('contactus', views.contactus, name='contactus'),
+    path('logout', views.logoutU,name = 'logout'),
+    path('signup.html', views.signup,name = 'signup.html'),
 ]
--- Home/views.py
+++ Home/views.py
@@ -5,10 +5,12 @@ from django.views.generic.list import ListView
 from .models import Contact
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DetailView
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
+from .forms import SignUpForm
+import os
 
 # Create your views here.
 def index(request):
@@ -24,3 +26,36 @@ def contactus(request):
         contactus.save()
         # print(name)
     return render(request, 'Contactus.html')
+
+def login1(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, 'index.html')
+        else:
+            return render(request, 'login.html')
+
+    return render(request, 'login.html')
+
+
+def logoutU(request):
+    auth.logout(request)
+    return render(request, 'index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
